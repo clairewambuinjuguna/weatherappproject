fca5bfb1dff1e75c6d98d2634738a8594895e27d
clairewambuinjuguna <clairenjugunaw@gmail.com>
2024-03-28 11:46:24 +0300

    added the day time feature

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
-const axios = require("axios");
-
-const apiKey = "f3793ob0fb2a47bb6356018f3e03t604";
+const apiKey = "047e82a3493503fd3349718fd1e42b74";
 const apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}";
-
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherDescription = document.querySelector(".weather-description");
+
 const cityElement = document.querySelector(".city");
 const tempElement = document.querySelector(".temp");
 const humidityElement = document.querySelector(".humidity");
 const windElement = document.querySelector(".wind");
+const dateTimeElement = document.querySelector(".date-time");
 
 async function checkWeather(city) {
-  try {
-    const response = await axios.get(apiUrl, {
-      params: {
-        query: city,
-        key: apiKey,
-      },
-    });
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-    const data = response.data.data;
-
-    document.querySelector(".weather").style.display = "block";
-    document.querySelector(".error").style.display = "none";
-
-    cityElement.innerHTML = data.city;
-    tempElement.innerHTML = Math.round(data.temp) + "°C";
-    humidityElement.innerHTML = data.humidity + "%";
-    windElement.innerHTML = data.wind.speed + "km/h";
-  } catch (error) {
+  if (!response.ok) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
-    console.error("Error fetching weather data:", error);
+    console.error("Error fetching weather data. HTTP Status:", response.status);
+    return;
   }
+
+  const data = await response.json();
+
+  if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "./assets/clouds.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "./assets/drizzle.png";
+  } else if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "./assets/clear.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "./assets/rainy.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "./assets/mist.png";
+  } else if (data.weather[0].main === "Snow") {
+    weatherIcon.src = "./assets/snow.png";
+  }
+
+  // Update weather description
+  weatherDescription.textContent = data.weather[0].description;
+
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".error").style.display = "none";
+
+  cityElement.innerHTML = data.name;
+  tempElement.innerHTML = Math.round(data.main.temp) + "°C";
+  humidityElement.innerHTML = data.main.humidity + "%";
+  windElement.innerHTML = data.wind.speed + "km/h";
+
+  dateTimeElement.innerHTML = formatDate(new Date());
 }
 
 searchBtn.addEventListener("click", () => {
@@ -45,3 +62,30 @@ searchBox.addEventListener("keypress", (event) => {
     checkWeather(searchBox.value.trim());
   }
 });
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
